6c4d21a05dd53ffe72f44f7bb395372703b2eec7
Christoph Thurnheer <43876727+Thurnheer@users.noreply.github.com>
2021-12-11 16:30:53 +0100

    Initialize add_posibility_to_add_dependencies_as_system_headers

---
+++ cmake/TargetLinkSystem.cmake
@@ -0,0 +1,8 @@
+function(target_link_libraries_system target scope)
+    set(libs ${ARGN})
+    foreach(lib ${libs})
+        get_target_property(lib_include_dir ${lib} INTERFACE_INCLUDE_DIRECTORIES)
+        target_include_directories(${target} SYSTEM ${scope} ${lib_include_dir})
+        target_link_libraries(${target} ${scope} ${lib})
+    endforeach(lib)
+endfunction(target_link_libraries_system)
